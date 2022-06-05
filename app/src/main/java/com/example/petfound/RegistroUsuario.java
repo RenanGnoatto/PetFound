9cc743d1db02dcfa5f421b5babec2e4b5ebb2259
RenanGnoatto <renan.gnoatto@hotmail.com>
2022-06-04 21:36:50 -0300

    Implementação de registro de usuário

--- app/src/main/java/com/example/petfound/RegistroUsuario.java
+++ app/src/main/java/com/example/petfound/RegistroUsuario.java
@@ -1,30 +1,41 @@
 package com.example.petfound;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 public class RegistroUsuario extends AppCompatActivity {
 
-    private EditText edtEmailRegistro;
+    private EditText edtNomeUsuarioRegistro;
     private Spinner sCidade;
+    private EditText edtEmailRegistro;
     private EditText edtSenhaRegistro;
     private EditText edtSenhaRegistroConfirmar;
     private CheckBox cbSenhaRegistroMostrar;
     private Button btRegistrarUsuario;
     private Button btCancelarRegistroUsuario;
     private SQLiteDatabase db = null;
+    private AlertDialog alerta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +43,14 @@ public class RegistroUsuario extends AppCompatActivity {
         setContentView(R.layout.activity_registro_usuario);
         db = new DatabaseManager(this, "BancoDados", null, 1).getWritableDatabase();
 
-        edtEmailRegistro = findViewById(R.id.edt_email_registro);
-        sCidade = findViewById(R.id.s_cidade);
-        edtSenhaRegistro = findViewById(R.id.edt_senha_registro);
-        edtSenhaRegistroConfirmar = findViewById(R.id.edt_senha_registro_confirmar);
-        cbSenhaRegistroMostrar = findViewById(R.id.cb_senha_registro_mostrar);
-        btRegistrarUsuario = findViewById(R.id.bt_registrar_usuario);
-        btCancelarRegistroUsuario = findViewById(R.id.bt_cancelar_registro_usuario);
+        edtNomeUsuarioRegistro = (EditText) findViewById(R.id.edt_nome_usuario_registro);
+        sCidade = (Spinner) findViewById(R.id.s_cidade);
+        edtEmailRegistro = (EditText) findViewById(R.id.edt_email_registro);
+        edtSenhaRegistro = (EditText) findViewById(R.id.edt_senha_registro);
+        edtSenhaRegistroConfirmar = (EditText) findViewById(R.id.edt_senha_registro_confirmar);
+        cbSenhaRegistroMostrar = (CheckBox) findViewById(R.id.cb_senha_registro_mostrar);
+        btRegistrarUsuario = (Button) findViewById(R.id.bt_registrar_usuario);
+        btCancelarRegistroUsuario = (Button) findViewById(R.id.bt_cancelar_registro_usuario);
 
         populaCidade(sCidade);
 
@@ -48,12 +60,61 @@ public class RegistroUsuario extends AppCompatActivity {
                 finish();
             }
         });
+
+        btRegistrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtNomeUsuarioRegistro.getText() != null &&
+                sCidade.getSelectedItemPosition() > 0 &&
+                edtEmailRegistro.getText() != null &&
+                edtSenhaRegistro.getText() != null &&
+                edtSenhaRegistroConfirmar.getText() != null) {
+                    if (edtSenhaRegistro.getText().toString().equals(edtSenhaRegistroConfirmar.getText().toString())) {
+                        db.execSQL("insert into usuario " +
+                                "(nome, email, senha, id_cidade) " +
+                                "values ('" + edtNomeUsuarioRegistro.getText() + "','" + edtEmailRegistro.getText() +
+                                "','" + edtSenhaRegistro.getText() + "'," + (int) sCidade.getSelectedItemId() + ")");
+                        edtNomeUsuarioRegistro.setText("");
+                        sCidade.setId(0);
+                        edtEmailRegistro.setText("");
+                        edtSenhaRegistro.setText("");
+                        edtSenhaRegistroConfirmar.setText("");
+                        cbSenhaRegistroMostrar.setChecked(false);
+                        /*Snackbar sbCadastroRealizado = Snackbar.make(findViewById(R.id.CoordinatorLayout),"Registro realizado com sucesso!",Snackbar.LENGTH_SHORT);
+                        sbCadastroRealizado.show();*/
+                    }
+                    else {
+                        Snackbar sbSenhasDiferentes = Snackbar.make(findViewById(R.id.CoordinatorLayout),"As senhas informadas estão diferentes!",Snackbar.LENGTH_SHORT);
+                        sbSenhasDiferentes.show();
+                    }
+                }
+                else {
+                    Snackbar sbCamposObrigatorios = Snackbar.make(findViewById(R.id.CoordinatorLayout),"Todos os campos são obrigatórios!",Snackbar.LENGTH_SHORT);
+                    sbCamposObrigatorios.show();
+                }
+            }
+        });
+
+        cbSenhaRegistroMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cbSenhaRegistroMostrar.isChecked()) {
+                    edtSenhaRegistro.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    edtSenhaRegistroConfirmar.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else {
+                    edtSenhaRegistro.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    edtSenhaRegistroConfirmar.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
     }
 
-    public Spinner populaCidade(Spinner sCidade) {
+    private Spinner populaCidade(Spinner sCidade) {
         ArrayList<String> listaCidades = new ArrayList<>();
-        Cursor cur = db.rawQuery("select nome, estado from cidade", null);
-        int contador = 0;
+        Cursor cur = db.rawQuery("select nome, estado from cidade order by id", null);
+        int contador = 1;
+        listaCidades.add(0,"Selecione uma cidade");
         while (cur.moveToNext()) {
             listaCidades.add(contador, cur.getString(0) + " - " + cur.getString(1) );
             contador ++;
@@ -62,5 +123,18 @@ public class RegistroUsuario extends AppCompatActivity {
         a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sCidade.setAdapter(a);
         return sCidade;
+    }
+
+    private void alertaUsuarioCadastrado() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Cadastro de Usuário");
+        builder.setMessage("Usuário cadastrado com sucesso!");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(RegistroUsuario.this, "positivo=" + arg1, Toast.LENGTH_SHORT).show();
+            }
+        });
+        alerta = builder.create();
+        alerta.show();
     }
 }
